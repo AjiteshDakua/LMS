50ad0f7073d7206f6be784789a731de0bc938032
AjiteshDakua <ajiteshdakua8@gmail.com>
2025-02-23 00:36:20 +0530

    change incorrect body

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
 // connect to the mongoDb database
-const connectDB =async () => {
-    mongoose.connection.on('connected',()=>console.log('Database Connected')
-    )
+const connectDB = async () => {
+  mongoose.connection.on("connected", () => console.log("Database Connected"));
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/LMS`);
-    
-}
-export default connectDB
+  await mongoose.connect(`${process.env.MONGODB_URI}/LMS`);
+};
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
-// Initialize Express 
-const app = express()
+// Initialize Express
+const app = express();
 
-// connect db 
-await connectDB()
+// connect db
+await connectDB();
 
 //MiddleWaress
 app.use(cors());
 
-//Routes 
-app.get('/', (req,res)=>res.send("API WORKING"));
-app.post('/clerk', express.json(),clerkWebhooks);
+//Routes
+app.get("/", (req, res) => res.send("API WORKING"));
+app.post("/clerk", express.json(), clerkWebhooks);
 
-//port 
+//port
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
 //
